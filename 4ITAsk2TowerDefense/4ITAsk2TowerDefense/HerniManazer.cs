b847eeebcf26b7a457c331d04f7f4be38ea4df9b
Matav5 <verner.matej@gmail.com>
2023-12-07 10:14:28 +0100

    HerniManazer.cs

---
+++ 4ITAsk2TowerDefense/4ITAsk2TowerDefense/HerniManazer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ITAVazy
+{
+    internal class HerniManazer
+    {
+        /// <summary>
+     /// Bude obsahovat
+     ///        seznamZaku
+     /// 
+     ///        metodu Prohra
+     ///        metodu ZiskejZaka
+     ///        metodu OnZakZemrel
+     ///        metodu VytvorVlnu
+     /// </summary>
+
+    }
+}
